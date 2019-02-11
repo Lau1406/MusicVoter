aec1c3115ec843fb7d0ea2c5709aab405a9d6998
laurence <dev@lkeijzer.nl>
2019-02-11 17:02:04 +0100

    add Voter app

--- MusicVoter/urls.py
+++ MusicVoter/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, re_path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^accounts/', include('allauth.urls')),
+    path('', include('Voter.urls')),
 ]
