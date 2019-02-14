f57e9dded505d0faca2852314e942ecbfabf213f
laurence <dev@lkeijzer.nl>
2019-02-14 16:48:47 +0100

    update models

--- Voter/models.py
+++ Voter/models.py
@@ -5,15 +5,21 @@ from django.utils.translation import ugettext
 
 class Artist(models.Model):
     name = models.CharField(max_length=200)
-    uri = models.CharField(max_length=200)
+    uri = models.CharField(max_length=200, primary_key=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Song(models.Model):
     name = models.CharField(max_length=200)
     image = models.CharField(max_length=1000)
-    artists = models.ManyToManyField(Artist)
+    artists = models.ManyToManyField(Artist, blank=True)
     length_ms = models.PositiveIntegerField()
-    uri = models.CharField(max_length=200)
+    uri = models.CharField(max_length=200, primary_key=True)
+
+    def __str__(self):
+        return self.name
 
     # def vote(self, vote: Vote):
     #     # TODO: implement actual voting procedure
@@ -34,3 +40,6 @@ class Vote(models.Model):
     user = models.ForeignKey(USER_MODEL, on_delete=models.CASCADE)
     upvote = models.CharField(max_length=1, choices=VOTES, default=NEUTRAL)
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.attname
