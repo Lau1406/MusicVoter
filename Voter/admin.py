d3a23027bfc9fba1289eaf42e88370b12b76503b
laurence <dev@lkeijzer.nl>
2019-02-14 15:39:19 +0100

    add basic models

--- Voter/admin.py
+++ Voter/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from Voter.models import *
 
 # Register your models here.
+
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    pass
--- Voter/models.py
+++ Voter/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from allauth.app_settings import USER_MODEL
+from django.utils.translation import ugettext
 
-# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+    uri = models.CharField(max_length=200)
+
+
+class Song(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.CharField(max_length=1000)
+    artists = models.ManyToManyField(Artist)
+    length_ms = models.PositiveIntegerField()
+    uri = models.CharField(max_length=200)
+
+    # def vote(self, vote: Vote):
+    #     # TODO: implement actual voting procedure
+    #     self.votes += 1
+
+
+class Vote(models.Model):
+    UPVOTE = '1'
+    NEUTRAL = '0'
+    DOWNVOTE = '-1'
+
+    VOTES = (
+        (UPVOTE, ugettext('Upvote')),
+        (NEUTRAL, ugettext('Neutral')),
+        (DOWNVOTE, ugettext('Downvote')),
+    )
+
+    user = models.ForeignKey(USER_MODEL, on_delete=models.CASCADE)
+    upvote = models.CharField(max_length=1, choices=VOTES, default=NEUTRAL)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
