fbc390aeb78c5de5931319792c4a4414a87a789f
laurence <dev@lkeijzer.nl>
2019-02-16 19:34:56 +0100

    add debug html code + moved templates dir to correct location

--- MusicVoter/settings.py
+++ MusicVoter/settings.py
@@ -144,7 +144,7 @@ ROOT_URLCONF = 'MusicVoter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'Voter/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
