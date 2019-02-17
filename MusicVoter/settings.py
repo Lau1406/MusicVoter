ccf3e3a861d97009e116142dc08a40807695829d
laurence <dev@lkeijzer.nl>
2019-02-17 11:53:42 +0100

    add redis backend

--- MusicVoter/settings.py
+++ MusicVoter/settings.py
@@ -228,3 +228,11 @@ SOCIALACCOUNT_PROVIDERS = {
 
 # Channels
 ASGI_APPLICATION = "MusicVoter.routing.application"
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
